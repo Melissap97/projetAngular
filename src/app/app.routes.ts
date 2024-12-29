d1c6158cfaa56772a5ed596b18506d23cc8393cb
Melissa <melissa.pegas@hotmail.fr>
2024-12-29 17:25:44 +0100

    ajout des routes et not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { CommandesComponent } from './pages/commandes/commandes.component';
+import { ListeUsersComponent } from './pages/liste-users/liste-users.component';
+import { ProduitsComponent } from './pages/produits/produits.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'commandes',
+    component: CommandesComponent
+},{
+    path:'users',
+    component: ListeUsersComponent
+},{
+    path:'produits',
+    component: ProduitsComponent
+},{
+    path:'**',
+    component: NotFoundComponent
+},
+
+];
