ccd9168f15e2c5d721e4d136cf30c0e4715986a4
Adrien <adrienribeiro999@gmail.com>
2024-12-30 16:17:34 +0100

    CSS connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConnexionComponent } from "./connexion/connexion.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ConnexionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
